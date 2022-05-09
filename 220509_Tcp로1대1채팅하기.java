753eb6bae24a8f4fa379e77fbe51764fe57ef328
Dasom Lee <101045176+dasom0625@users.noreply.github.com>
2022-05-09 17:22:30 +0900

    Update and rename 220509_TcpSender&TcpReciever.java to 220509_Tcp로1대1채팅하기.java

--- 220509_Tcp로1대1채팅하기.java
+++ 220509_Tcp로1대1채팅하기.java
@@ -93,3 +93,77 @@ public class Receiver extends Thread {
 	}
 	}	
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Socket객체를 생성하여 서버에 연결 요청을 보내고
+//연결이 완료되면 이 Socket을 메세지를 받는 쓰레드와 메세지를 보내는 쓰레드에 주입한다.
+package kr.or.ddit.basic.tcp;
+
+import java.net.Socket;
+
+public class TcpClient02 {
+
+	public static void main(String[] args) {
+
+		
+		try {
+			Socket socket = new Socket("192.168.46.19",7777); //new Socket(서버의 ip주소 ,port번호);
+			System.out.println("서버에 연결되었습니다..");
+			System.out.println();
+
+			Sender sender = new Sender(socket);
+			Receiver receiver = new Receiver(socket);
+			
+			sender.start();
+			receiver.start();
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+//ServerSocket 객체를 생성하고 클라이언트가 접속해 오면 
+//연결된 Socket을 메세지를 받는 쓰레드와 메세지를 보내는 쓰레드에 주입한다. 
+
+
+package kr.or.ddit.basic.tcp;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TcpServer02 {
+
+	public static void main(String[] args) throws IOException {
+		
+		
+		//서버 socket만들기
+		ServerSocket server = new ServerSocket(7777);
+
+		System.out.println("서버가 준비중입니다...");
+		System.out.println();
+		
+		Socket socket = server.accept(); //클라이언트의 접속을 기다린다.
+		
+		//접속이 되면 연결된 Socket객체를 주입한 쓰레드를 생성하여 실행한다.
+		Sender sender = new Sender(socket);
+		Receiver receiver = new Receiver(socket);
+		
+		//쓰레드 작동
+		sender.start();
+		receiver.start();
+	}
+
+}
